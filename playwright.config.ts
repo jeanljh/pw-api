bfcd4996dc9c9b94c277176b6b3774b9f3196648
Lee Jean Hwa <leejeanhwa@gmail.com>
2023-04-05 10:36:33 +0800

    init

---
+++ playwright.config.ts
@@ -0,0 +1,12 @@
+// playwright.config.ts
+import { defineConfig } from '@playwright/test';
+export default defineConfig({
+    reporter: 'html',
+    use: {
+        baseURL: 'https://ott-details.p.rapidapi.com/search',
+        extraHTTPHeaders: {
+            'X-RapidAPI-Key': '367e2d391cmsha1d5ce075ea7419p11b8ddjsnfa27b3c69464',
+            'X-RapidAPI-Host': 'ott-details.p.rapidapi.com'
+        },
+  }
+});
